78356a76403df7c02c78b61da248ab5e8cc2c30e
alexfrodev <alex.franco3d@outlook.com>
2024-11-05 21:46:58 -0600

    added LeafNode child class with test

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -12,10 +12,23 @@ class HTMLNode:
     def props_to_html(self):
         if isinstance(self.props, dict):
             result = "".join(f' {key}="{value}"' for key, value in self.props.items())
-            print(result)
             return result
-
+        return ""
 
 
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag=None, value=None, props=None):
+        super().__init__(tag=tag, value=value, props=props)
+
+    def to_html(self):
+        prop = super().props_to_html()
+        if self.value == None:
+            raise ValueError
+        if self.tag == None:
+            return self.value
+        else:
+            return f"<{self.tag}{prop}>{self.value}</{self.tag}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,17 +1,34 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_node_01(self):
         test_dict = {"href": "https://www.google.com","target": "_blank",}
         node_01 = HTMLNode(props=test_dict)
-        node_01.props_to_html()
+        result = node_01.props_to_html()
+        print(result)
 
     def test_node_02(self):
         test_dict = {"href": "https://www.google.com",}
         node_02 = HTMLNode(props=test_dict)
-        node_02.props_to_html()
+        result = node_02.props_to_html()
+        print(result)
+
+    def test_node_03(self):
+        node_03 = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        result = node_03.to_html()
+        print(result)
+
+    def test_node_04(self):
+        node_03 = LeafNode("p", "This is a paragraph of text.")
+        result = node_03.to_html()
+        print(result)
+
+    def test_node_05(self):
+        node_03 = LeafNode(tag=None, value="This is a paragraph of text.")
+        result = node_03.to_html()
+        print(result)
 
 
 if __name__ == "main":
